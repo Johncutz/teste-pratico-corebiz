2b692ea440bde721ee48716a3886d46009c9ea95
Johncutz <joaovitorcut@gmail.com>
2022-07-06 22:14:53 -0300

    Adicionando Hamburger Icon

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,11 +2,10 @@ import React from "react";
 import './Header.css';
 
 
-import Badge from "@material-ui/core/Badge";
 
 import logo from '../../assets/img/Corebiz.png';
 import lupa from '../../assets/img/Lupa.png';
-import carrinho from '../../assets/img/Carrinho.png';
+import hamburger from '../../assets/img/Hamburger-icon.svg';
 import user from '../../assets/img/User.png';
 
 function Header() {
@@ -17,6 +16,7 @@ function Header() {
             <input className="header__input" type="text" placeholder="O que você está precisando?" />
             <img className="header__lupa" src={lupa} alt="Lupa" />
             <button className="header__conta" type="button">Minha Conta!</button>
+            <img className="header__hamburger" src={hamburger}/>
             <img className="header__user" src={user} />
             <button className="header__carrinho" type="button"></button>
         </header>
